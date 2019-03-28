1954cfd9354c12cd6ad14c36b3d25b47348fe759
summer <46586958+xtdf1106@users.noreply.github.com>
2019-03-29 01:04:23 +0800

    no message (#3)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express dev ' });
+  res.render('index', { title: 'Express Master bug fix 111' });
 });
 
 module.exports = router;

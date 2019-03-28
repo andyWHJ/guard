cc26511440e3820409d191e14be266cc0f7b0d76
王浩杰 <haojie.wang@ly.com>
2019-03-29 01:47:27 +0800

    no message

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.post('/movie', function(req, res, next) {
+	conosle.log(JSON.stringify(req.body));
 	res.json(req.body);
 });
 

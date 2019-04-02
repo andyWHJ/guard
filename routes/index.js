4d9401c66eab0715a508357f898a19a4b65fb153
王浩杰 <haojie.wang@ly.com>
2019-04-02 08:10:16 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Master bug fix 222' });
-
-  
+  res.render('index', { title: 'Express Master bug fix 222' });  
 });
 
 module.exports = router;

3596390bcc78814bff475450900c18ae10fcd586
272267016 <272267016@qq.com>
2019-05-06 16:51:39 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log('new')
   res.render('index', { title: 'Express Master bug fix 222 gmail 111 2222 new branch 19-5-6-new' });
 });
 

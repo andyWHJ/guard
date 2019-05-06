612ddee5a32de94e2dea6f0ed0d8af54711442d5
xtdf1106 <xtdf1106@gmail.com>
2019-05-06 16:32:44 +0800

    aaa

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Master bug fix 222 gmail' });
+  res.render('index', { title: 'Express Master bug fix 222 gmail 111' });
 });
 
 module.exports = router;
